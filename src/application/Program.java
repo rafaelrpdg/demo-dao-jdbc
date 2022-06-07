db082e67551096c0c87fd1c2ad0e6bf5f72bea92
Rafael Rodrigues Pereira da Graça <raphapereira2011@gmail.com>
2022-06-07 04:16:33 -0300

    Reusando a Instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,14 +9,12 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "book");
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== Test FindById ===");
 		Seller seller = sellerDao.findById(3);
 		
-		System.out.println(obj);
-		
-		//seller = new Seller(21,"Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(seller);
 
 	}

377a234dfdd7e201d89560ace8c687e10d45930c
Rafael Rodrigues Pereira da Graça <raphapereira2011@gmail.com>
2022-06-06 21:55:26 -0300

    FindbyID impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,5 @@
 package application;
-import java.util.*;
+import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,10 +12,11 @@ public class Program {
 		Department obj = new Department(1, "book");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(obj);
 		
-		Seller seller = new Seller(21,"Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+		//seller = new Seller(21,"Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(seller);
 
 	}

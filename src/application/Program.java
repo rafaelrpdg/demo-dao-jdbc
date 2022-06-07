fa4d09cc373c58bd61e61fc52a401eef8a9543bb
Rafael Rodrigues Pereira da Graça <raphapereira2011@gmail.com>
2022-06-06 21:00:23 -0300

    Criando impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 import java.util.*;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -7,6 +10,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "book");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		
 		System.out.println(obj);
 		
 		Seller seller = new Seller(21,"Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
